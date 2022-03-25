c16f275ab215d0f3eefe8ea144b23126d031be46
Ozodjon-dev <viperson.333@gmail.com>
2022-03-25 17:56:37 +0500

    updated http://127.0.0.1:8000/posts/create page

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,74 +1,82 @@
 @extends('layouts.main')
 @section('content')
-<body class="bg-light">
-    
-<div class="container">
-  <main>
-    <div class="py-5 text-center">
-      <img class="d-block mx-auto mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-      <h2>Contracts</h2>
-      <p class="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
-    </div>
-
-    <div class="row g-5">
-      <div class="">
-        <h4 class="mb-3">Billing address</h4>
-        <form class="needs-validation" action="{{ route('posts.store') }}" method="post">
-          @csrf
-          <div class="row g-3">
-            <div class="col-sm-6">
-              <label for="firstName" class="form-label">First name</label>
-              <input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
-              <div class="invalid-feedback">
-                Valid first name is required.
+  <body class="bg-light">
+      
+    <div class="container">
+      <main>
+        <div class="py-5 text-center">
+          <h2>Contracts</h2>
+        </div>
+        <div>
+          <div class="">
+            <h4 class="mb-3">Billing address</h4>
+            <form class="needs-validation" action="{{ route('posts.store') }}" method="post">
+              @csrf
+              <div class="row g-4">
+              <div class="col-sm-6">
+                <label for="firstName" class="form-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="" value="" required="">
+                <div class="invalid-feedback">
+                  Valid first name is required.
+                </div>
               </div>
-            </div>
 
-            <div class="col-sm-6">
-              <label for="lastName" class="form-label">Last name</label>
-              <input type="text" class="form-control" id="lastName" placeholder="" value="" required="">
-              <div class="invalid-feedback">
-                Valid last name is required.
+              <div class="col-sm-6">
+                <label for="lastName" class="form-label">Content</label>
+                <input type="text" class="form-control" id="content" name="content" placeholder="" value="" required="">
+                <div class="invalid-feedback">
+                  Valid last name is required.
+                </div>
               </div>
-            </div>
 
-            <div class="col-12">
-              <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-              <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
-            </div>
-
-            <div class="col-md-3">
-              <label for="zip" class="form-label">Zip</label>
-              <input type="text" class="form-control" id="zip" placeholder="" required="">
-              <div class="invalid-feedback">
-                Zip code required.
+              <div class="col-sm-6">
+                <label for="firstName" class="form-label">Image</label>
+                <input type="text" class="form-control" id="image" name="image" name="title" placeholder="" value="" required="">
+                <div class="invalid-feedback">
+                  Valid first name is required.
+                </div>
               </div>
-            </div>
+
+              <div class="col-sm-6">
+                <label for="lastName" class="form-label">Likes</label>
+                <input type="text" class="form-control" id="content" name="content" placeholder="" value="" required="">
+                <div class="invalid-feedback">
+                  Valid last name is required.
+                </div>
+              </div>
+
+              <div class="col-sm-6">
+                <label for="firstName" class="form-label">Is published</label>
+                <input type="int" class="form-control" id="image" name="is_published" name="is_published" placeholder="" value="" required="">
+                <div class="invalid-feedback">
+                  Valid is published is required.
+                </div>
+              </div>
+
+              <div class="mt-3 col-md-12">
+                <button class="w-100 btn btn-primary btn-lg mt-3" type="submit">Continue to checkout</button>                
+              </div>
+
+
+            </form>
           </div>
+        </div>
 
-          <hr class="my-4">
+      </main>
 
-          <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
-        </form>
-      </div>
+      <footer class="my-5 pt-5 text-muted text-center text-small">
+        <p class="mb-1">© 2017–2021 Company Name</p>
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="#">Privacy</a></li>
+          <li class="list-inline-item"><a href="#">Terms</a></li>
+          <li class="list-inline-item"><a href="#">Support</a></li>
+        </ul>
+      </footer>
     </div>
-  </main>
-
-  <footer class="my-5 pt-5 text-muted text-center text-small">
-    <p class="mb-1">© 2017–2021 Company Name</p>
-    <ul class="list-inline">
-      <li class="list-inline-item"><a href="#">Privacy</a></li>
-      <li class="list-inline-item"><a href="#">Terms</a></li>
-      <li class="list-inline-item"><a href="#">Support</a></li>
-    </ul>
-  </footer>
-</div>
-
 
     <script src="js/bootstrap.bundle.min.js"></script>
 
     <script src="js/form-validation.js"></script>
-  
-
-</body>
+      
+  </body>
 @endsection
