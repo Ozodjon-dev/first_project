5dab6a77833aa86c1f2562704be5bbfa2a541c3c
Ozodjon-dev <100852227+Ozodjon-dev@users.noreply.github.com>
2022-07-12 20:47:19 +0500

    added model Post to PostFactory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,9 +3,11 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
 
 class PostFactory extends Factory
 {
+    protected $model = Post::class;
     /**
      * Define the model's default state.
      *
