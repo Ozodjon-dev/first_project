650e133362c37bac9a41a9b522c573a9247f38a3
Ozodjon-dev <100852227+Ozodjon-dev@users.noreply.github.com>
2022-07-23 22:55:49 +0500

    find information in the database with a part of the attribute

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -5,13 +5,28 @@ use App\Http\Controllers\Controller;
 use App\Models\Post;
 use App\Models\Tag;
 use App\Models\Category;
+use App\Http\Controllers\Post\FilterRequest;
+use App\Http\Requests\Post\FilterRequest as PostFilterRequest;
 
 class IndexController extends BaseController
 {
-    public function __invoke()
+    public function __invoke(PostFilterRequest $request)
     {
-        $posts = post::paginate(10);
-        return view('post.index', compact('posts'));
+        $data = $request->validated();
+
+        $query = Post::query();
+        if (isset($data['category_id'])){
+            $query->where('category_id', $data['category_id']);
+        }
+        if (isset($data['title'])){
+            $query->where('title', 'like', "%{$data['title']}%");
+        }
+        $posts = $query->get();
+        dd($posts);
+        // $posts = post::paginate(8);
+        // return view('post.index', compact('posts'));
+
+
 
         // $category = Category::find(1);
         // $tag = Tag::find(3);
